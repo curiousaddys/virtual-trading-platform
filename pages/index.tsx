8274cc75149f91898e37f4f9c7c37756345efdf0
Jake Hobbs <jakehobbs@gmail.com>
2021-12-22 22:48:12 -0800

    add tailwind & create basic price table

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,21 @@ import type { NextPage } from 'next'
 import Image from 'next/image'
 import Head from 'next/head'
 import { useEffect, useState } from 'react'
+import React from 'react'
 import { GeckoPrices } from './api/prices'
 import ky from 'ky'
+import { Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts'
 
 const Home: NextPage = () => {
-  // TODO(jh): maybe make a hook like you did for ALC API?
   const [isLoading, setIsLoading] = useState(true)
+  // TODO(jh): maybe make a hook like you did for ALC API?
   const [prices, setPrices] = useState<GeckoPrices[] | null>(null)
 
   useEffect(() => {
+    // TODO(jh): handle errors
     ;(async () => {
       const data = await ky.get('/api/prices').json<GeckoPrices[]>()
+      // TODO(jh): remove logging
       console.log(data)
       setPrices(data)
       setIsLoading(false)
@@ -27,47 +31,123 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <div className="container flex justify-center mx-auto">
         {isLoading ? (
           <p>Loading...</p>
         ) : (
-          <table>
-            <thead>
-              <tr>
-                <th>Logo</th>
-                <th>Name</th>
-                <th>Current Price</th>
-                <th>Price Change (24 hr)</th>
-                <th>TODO</th>
-              </tr>
-            </thead>
+          <div className="flex flex-col">
+            <div className="w-full">
+              <div className="shadow rounded-lg border my-20">
+                {/*TODO: style table nicer*/}
+                <table className="table-auto w-full">
+                  <thead className="bg-gray-50">
+                    <tr>
+                      <th className="px-4 py-2 text-xs text-gray-500 rounded-tl-lg">
+                        Name
+                      </th>
+                      <th className="px-4 py-2" />
+                      <th className="px-6 py-2 text-xs text-gray-500 text-right">
+                        Price
+                      </th>
+                      <th className="px-6 py-2 text-xs text-gray-500 text-right">
+                        Change (1 day)
+                      </th>
+                      <th className="px-6 py-2 rounded-tr-lg" />
+                    </tr>
+                  </thead>
 
-            <tbody>
-              {prices &&
-                prices.map((coin) => (
-                  <tr key={coin.id}>
-                    <td>
-                      <Image
-                        src={coin.image}
-                        height={40}
-                        width={40}
-                        alt={coin.symbol}
-                      />
-                    </td>
-                    <td>{coin.name}</td>
-                    <td>${coin.current_price}</td>
-                    <td>
-                      {/*TODO(jh): red is negative, green if positive*/}
-                      {Math.round(coin.price_change_percentage_24h * 100) / 100}
-                      %
-                    </td>
-                    <td>chart</td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
+                  <tbody className="bg-white">
+                    {prices &&
+                      prices.map((coin) => (
+                        <tr
+                          key={coin.id}
+                          className="whitespace-nowrap even:bg-gray-50"
+                        >
+                          <td className="px-4 py-4">
+                            <Image
+                              src={coin.image}
+                              height={40}
+                              width={40}
+                              alt={coin.symbol}
+                            />
+                          </td>
+                          <td className="px-4 py-4 text-sm text-gray-900 font-bold">
+                            {coin.name}{' '}
+                            <span className="font-light ml-3">
+                              {coin.symbol.toUpperCase()}
+                            </span>
+                          </td>
+                          <td className="px-6 py-4 text-sm text-gray-500 text-right">
+                            $
+                            {coin.current_price.toLocaleString('en-US', {
+                              minimumFractionDigits: 2,
+                              maximumFractionDigits: 2,
+                            })}
+                          </td>
+                          <td
+                            className={`px-6 py-4 text-sm text-gray-500 text-right ${
+                              coin.price_change_percentage_24h >= 0
+                                ? 'text-green-500'
+                                : 'text-red-500'
+                            }`}
+                          >
+                            {coin.price_change_percentage_24h.toLocaleString(
+                              'en-US',
+                              {
+                                minimumFractionDigits: 2,
+                                maximumFractionDigits: 2,
+                                signDisplay: 'always',
+                              }
+                            )}
+                            %
+                          </td>
+                          <td>
+                            <LineChart
+                              width={200}
+                              height={75}
+                              data={(coin.sparkline_in_7d.price ?? [0])
+                                .slice(
+                                  (coin.sparkline_in_7d.price ?? [0]).length -
+                                    25,
+                                  coin.sparkline_in_7d.price?.length
+                                )
+                                .map((x) => ({
+                                  x,
+                                }))}
+                              margin={{
+                                top: 10,
+                                right: 10,
+                                bottom: 10,
+                                left: 10,
+                              }}
+                            >
+                              <YAxis
+                                type="number"
+                                domain={['dataMin', 'dataMax']}
+                                hide
+                              />
+                              <Line
+                                type="linear"
+                                dataKey="x"
+                                stroke={
+                                  coin.price_change_percentage_24h >= 0
+                                    ? '#22c55e'
+                                    : '#ef4444'
+                                }
+                                dot={false}
+                                isAnimationActive={false}
+                              />
+                            </LineChart>
+                          </td>
+                        </tr>
+                      ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
         )}
-      </main>
+      </div>
     </div>
   )
 }
