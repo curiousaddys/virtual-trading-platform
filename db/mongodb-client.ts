d025a81acf76444d0be1c50fc4bef3fcea55d3b2
Jake Hobbs <jakehobbs@gmail.com>
2021-12-29 17:03:51 -0800

    db connection improvements

--- db/mongodb-client.ts
+++ db/mongodb-client.ts
@@ -1,7 +1,8 @@
-import { ClientSession, MongoClient } from 'mongodb'
+import { ClientSession, Db, MongoClient } from 'mongodb'
 import { config } from '../utils/config'
 
 let client: MongoClient | null = null
+let db: Db | null = null
 let session: ClientSession | null = null
 
 export const getMongoDB = async () => {
@@ -10,12 +11,16 @@ export const getMongoDB = async () => {
       config.MONGO_URI,
       config.MONGO_OPTIONS
     ).connect()
+    db = client.db()
+    console.log('🔥 New DB Connection')
   }
+
   // Using a session prevents cursor ID errors.
   // See: https://jira.mongodb.org/browse/NODE-3521
   if (!session) {
     session = client.startSession()
+    console.log('🔥 New DB Session')
   }
-  const db = client.db()
+
   return { db, session }
 }
