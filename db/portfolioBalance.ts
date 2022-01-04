671eeccc1f68762407f52cb52f7f63fcae5419fa
Jake Hobbs <jakehobbs@gmail.com>
2022-01-03 16:55:41 -0800

    clean up

--- db/portfolioBalance.ts
+++ db/portfolioBalance.ts
@@ -16,18 +16,14 @@ const getCollection = {
 }
 
 export const getPortfolioBalanceHistory = async (portfolioID: ObjectID, days: DateRangeValues) => {
-  // TODO: add params for time range (today, this week, 30 days, 365 days, all)
-  // for today (minutely)
   const { collection, session } = await getCollection[days]()
-  // TODO: filter timestamp greater than or equal to whatever
-  const startDate = days === 'max' ? null : dayjs().subtract(parseInt(days), 'day').toDate()
+  const startDate = (
+    days === 'max' ? dayjs('1970-01-01') : dayjs().subtract(parseInt(days), 'day')
+  ).toDate()
   const results = await collection.find(
-    { portfolioID, timestamp: { $gte: startDate ?? dayjs('1970-01-01').toDate() } },
+    { portfolioID, timestamp: { $gte: startDate } },
     { session }
   )
   const resultsArr = await results.toArray()
   return resultsArr.sort((a, b) => a.timestamp.getTime() - b.timestamp.getTime())
-  // for the week (hourly)
-  // for this year (daily)
-  // for all time (daily)
 }
