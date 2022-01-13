16d297b9b26a2a49fd1e13777ed0ab1505a2ce8e
Jake Hobbs <jakehobbs@gmail.com>
2022-01-13 14:34:11 -0800

    handle got http errors

--- utils/errors.ts
+++ utils/errors.ts
@@ -1,5 +1,6 @@
 import { ZodError } from 'zod'
 import { UnauthorizedError } from './auth'
+import { HTTPError } from 'got'
 
 export interface ErrResp {
   error: string
@@ -13,6 +14,9 @@ export const getErrorDetails = (err: any): { status: number; message: string } =
   if (err instanceof ZodError) {
     // TODO(jh): list all errors instead of just the 1st one
     return { status: 400, message: `${err.issues[0].path[0]}: ${err.issues[0].message}` }
+  }
+  if (err instanceof HTTPError) {
+    return { status: 500, message: `Got HTTP Error ${err.code}: ${err.message}` }
   }
   return { status: 500, message: 'unknown server error' }
 }
