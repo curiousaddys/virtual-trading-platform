35d692ad93d7de98dd6d2565b2ef6dd5d5c08101
Jake Hobbs <jakehobbs@gmail.com>
2022-01-20 18:29:10 -0800

    use memo instead of state

--- hooks/usePagination.ts
+++ hooks/usePagination.ts
@@ -1,18 +1,18 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useMemo, useState } from 'react'
 
 export const usePagination = <Type>(itemsPerPage: number) => {
   const [allItems, setAllItems] = useState<Type[]>([] as Type[])
-  const [currentItems, setCurrentItems] = useState<Type[]>([])
   const [pageNumber, setPageNumber] = useState(0)
+
   const pageCount = useMemo(() => {
     return Math.floor(allItems.length / itemsPerPage)
   }, [allItems.length, itemsPerPage])
 
-  useEffect(() => {
+  const currentItems = useMemo(() => {
     const startOffset = (pageNumber * itemsPerPage) % allItems.length
     const endOffset = startOffset + itemsPerPage
-    setCurrentItems(allItems.slice(startOffset, endOffset))
-  }, [allItems, allItems.length, itemsPerPage, pageNumber])
+    return allItems.slice(startOffset, endOffset)
+  }, [allItems, itemsPerPage, pageNumber])
 
   return { currentItems, setAllItems, pageCount, pageNumber, setPageNumber }
 }
