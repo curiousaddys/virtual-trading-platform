2f3ffa09b6453eaa7473d5d4b29843627d945f88
rayzr522 <rayzr522@gmail.com>
2022-07-07 23:24:51 -0400

    reorganize _app.tsx

---
+++ components/MetaTags.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export const MetaTags = () => (
+  <Head>
+    <title>Virtual Trading Platform</title>
+    <meta
+      name="description"
+      content="A virtual trading platform where you can explore the world of Cryptocurrency with no risk -- just log in with your wallet and get $100k free virtual USD to trade with!"
+    />
+    <link rel="icon" href="/favicon.ico" />
+  </Head>
+)
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,69 +1,69 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ExternalProvider, JsonRpcFetchFunc } from '@ethersproject/providers'
 import { Web3ReactProvider } from '@web3-react/core'
 import { ethers } from 'ethers'
-import { ExternalProvider, JsonRpcFetchFunc } from '@ethersproject/providers'
-import React from 'react'
-import { AccountContext, useAccount } from '../hooks/useAccount'
-import Head from 'next/head'
-import { Navbar } from '../components/Navbar'
+import type { AppProps } from 'next/app'
+import Script from 'next/script'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import { TEN_SEC_MS } from '../utils/constants'
-import { PricesContext, usePrices } from '../hooks/usePrices'
-import { BuySellModalContext, useBuySellModal } from '../hooks/useBuySellModal'
 import { BuySellModal } from '../components/BuySellModal'
-import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
-import { Partytown } from '@builder.io/partytown/react'
+import { MetaTags } from '../components/MetaTags'
+import { Navbar } from '../components/Navbar'
+import { AccountContext, useAccount } from '../hooks/useAccount'
+import { BuySellModalContext, useBuySellModal } from '../hooks/useBuySellModal'
+import { PricesContext, usePrices } from '../hooks/usePrices'
+import '../styles/globals.css'
+import { TEN_SEC_MS } from '../utils/constants'
 
 function getLibrary(provider: ExternalProvider | JsonRpcFetchFunc) {
   return new ethers.providers.Web3Provider(provider)
 }
 
+const GTM_ID = 'GTM-WP2MZ89'
+
 function MyApp({ Component, pageProps }: AppProps) {
   const account = useAccount()
   const prices = usePrices()
   const buySellModalState = useBuySellModal()
-  return (
-    <Web3ReactProvider getLibrary={getLibrary}>
-      <AccountContext.Provider value={account}>
-        <PricesContext.Provider value={prices}>
-          <BuySellModalContext.Provider value={buySellModalState}>
-            <Head>
-              <title>Virtual Trading Platform</title>
-              <meta name="description" content="Curious Addys' Trading Club" />
-              <link rel="icon" href="/favicon.ico" />
-              <Partytown
-                debug={process.env.NODE_ENV !== 'production'}
-                forward={['dataLayer.push']}
-              />
-              {/* eslint-disable-next-line @next/next/next-script-for-ga */}
-              <script
-                id="google-tag-manager"
-                type="text/partytown"
-                dangerouslySetInnerHTML={{
-                  __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                  })(window,document,'script','dataLayer','GTM-WP2MZ89');`,
-                }}
-              />
-            </Head>
-            <Navbar />
-            <Component {...pageProps} />
-            <BuySellModal />
-            <ToastContainer
-              position={'bottom-right'}
-              autoClose={TEN_SEC_MS}
-              newestOnTop={true}
-              theme={'colored'}
-            />
-          </BuySellModalContext.Provider>
-        </PricesContext.Provider>
-      </AccountContext.Provider>
-    </Web3ReactProvider>
+
+  let children = (
+    <>
+      <Script id="gtm">
+        {`
+          (function(w,d,s,l,i){
+            w[l]=w[l]||[];
+            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+      </Script>
+      <MetaTags />
+
+      <Navbar />
+      <Component {...pageProps} />
+
+      <BuySellModal />
+      <ToastContainer
+        position={'bottom-right'}
+        autoClose={TEN_SEC_MS}
+        newestOnTop={true}
+        theme={'colored'}
+      />
+    </>
   )
+  children = (
+    <BuySellModalContext.Provider value={buySellModalState}>
+      {children}
+    </BuySellModalContext.Provider>
+  )
+  children = <PricesContext.Provider value={prices}>{children}</PricesContext.Provider>
+  children = <AccountContext.Provider value={account}>{children}</AccountContext.Provider>
+  children = <Web3ReactProvider getLibrary={getLibrary}>{children}</Web3ReactProvider>
+
+  return children
 }
 
 export default MyApp
