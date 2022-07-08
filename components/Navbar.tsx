bc5f9416f99e076d264d559c04893533f2a58065
rayzr522 <rayzr522@gmail.com>
2022-07-07 23:30:12 -0400

    simplify navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,9 @@
-import Link from 'next/link'
-import { WalletConnector } from './WalletConnector'
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCog, faMedal } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Link from 'next/link'
+import React from 'react'
 import { useAccountContext } from '../hooks/useAccount'
+import { WalletConnector } from './WalletConnector'
 
 export const Navbar: React.VFC = () => {
   const { accountInfo } = useAccountContext()
@@ -11,36 +11,22 @@ export const Navbar: React.VFC = () => {
   return (
     <nav className="bg-white shadow-lg">
       <div className="max-w-6xl mx-auto px-4">
-        <div className="flex justify-between">
-          <div className="flex space-x-7">
-            <div>
-              <div className="flex items-center py-4 px-2 gap-2">
-                <span className="font-semibold text-gray-500 text-lg">
-                  <Link href="/">Virtual Trading Platform</Link>
-                </span>
-              </div>
-            </div>
-          </div>
-          <div className="flex items-center space-x-3 ">
+        <div className="flex">
+          <Link href="/" passHref>
+            <a className="py-4 px-2 mr-auto font-semibold text-gray-500 text-lg">
+              Virtual Trading Platform
+            </a>
+          </Link>
+          <div className="flex items-center gap-3">
             <Link href="/leaderboard" passHref>
               <a>
-                <FontAwesomeIcon
-                  icon={faMedal}
-                  color={'#FF8C00'}
-                  className="cursor-pointer"
-                  style={{ width: 24 }}
-                />
+                <FontAwesomeIcon icon={faMedal} color="#FF8C00" className="w-6" />
               </a>
             </Link>
             {accountInfo && (
               <Link href="/settings" passHref>
                 <a>
-                  <FontAwesomeIcon
-                    icon={faCog}
-                    color={'#00008B'}
-                    className="cursor-pointer"
-                    style={{ width: 24 }}
-                  />
+                  <FontAwesomeIcon icon={faCog} color="#00008B" className="w-6" />
                 </a>
               </Link>
             )}
