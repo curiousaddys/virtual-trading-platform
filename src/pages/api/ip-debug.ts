d94600337b93e36e5b637246879fff87babc84ca
jakehobbs <jakehobbs@gmail.com>
2023-01-10 13:32:16 -0800

    test custom ip function

--- src/pages/api/ip-debug.ts
+++ src/pages/api/ip-debug.ts
@@ -1,6 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { getClientIp } from 'request-ip'
+
+const getClientIp = (req: NextApiRequest) => {
+  if (!req.headers) {
+    return null
+  }
+  // If proxied behind Cloudflare, this will be the user's original IP.
+  const cloudflareConnectingIp = req.headers['cf-connecting-ip']
+  // Else, this header will be the user's original IP if connecting directly via Vercel.
+  const vercelRealIp = req.headers['x-real-ip']
+  return cloudflareConnectingIp ?? vercelRealIp ?? null
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const clientIp = getClientIp(req)
-  res.status(200).send(JSON.stringify({ clientIp, headers: req.headers }, undefined, 2))
+  res
+    .status(200)
+    .send(JSON.stringify({ clientIp, headers: req.headers, test: req.rawHeaders }, undefined, 2))
 }
